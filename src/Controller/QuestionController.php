feee0136cf7ca72532aef5f9cbeab710f5a1c13c
starkbaum <s.starkbaum@austrian-standards.at>
2020-11-19 17:17:48 +0100

    Chapter 3

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class QuestionController
+{
+    public function homepage()
+    {
+        return new Response('First Page');
+    }
+}
