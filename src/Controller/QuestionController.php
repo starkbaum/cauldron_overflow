07858d1fcaaf1a96c4597c39b29c79a87f110045
starkbaum <s.starkbaum@austrian-standards.at>
2020-11-20 08:02:25 +0100

    Chapter 9: twig

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -25,9 +26,20 @@ class QuestionController
      */
     public function show($slug): Response
     {
-        return new Response(sprintf(
-            'Future page to show the question %s',
-            ucwords(str_replace('-', ' ', $slug))
-        ));
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+        #return new Response(sprintf(
+        #    'Future page to show the question %s',
+        #    ucwords(str_replace('-', ' ', $slug))
+        #));
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers,
+        ]);
     }
 }
